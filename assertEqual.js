95225c527882eb59f203fdbabfc6bf28834ad6fa
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-03 19:59:25 +0000

    adding assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:" + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Passed:" + '"' + actual + '"' + " !== " + '"' + expected + '"');
+  }
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
