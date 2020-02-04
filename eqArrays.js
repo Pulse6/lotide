cc387709c67c3765435595e1dd9c551b80a87997
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-04 21:43:48 +0000

    adding eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -15,8 +15,8 @@ const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
   }
-  return true
-}
+  return true;
+};
 
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
 // assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)// => false
