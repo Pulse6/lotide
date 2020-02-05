b6b026e43907cee83fb0a180e89754e4cdbddede
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-05 01:30:53 +0000

    adding flatten.js

---
+++ flatten.js
@@ -0,0 +1,32 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:" + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Passed:" + '"' + actual + '"' + " !== " + '"' + expected + '"');
+  }
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)// => false
+
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+
+const flatten = function(arr){
+  let newArr = arr.flat()
+  return newArr;
+}
+
+// console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
