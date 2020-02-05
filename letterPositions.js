90485993304de079854053f3a215a213ff659ac4
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-05 20:32:26 +0000

    adding letterPosition.js

---
+++ letterPositions.js
@@ -0,0 +1,23 @@
+const letterPositions = function(sentence) {
+  // const noSpace = sentence.split(" ").join("");
+  const arr = sentence.split()
+  // console.log(arr)
+  const result = {};
+  // const entries = Object.entries(noSpace)
+  // console.log(entries)
+  // const keys = Object.keys(sentence)
+  // console.log(keys)
+  for (let i = 0; i < sentence.length; i++) {
+    // console.log(keys[i])
+    if (sentence[i] in result) {
+      result[sentence[i]].push(i)
+    } else {
+      result[sentence[i]] = [i]
+    }
+  }
+  // console.log(result)
+  delete result[" "]
+  return result;
+};
+
+console.log(letterPositions("lighthouse in the house"))
