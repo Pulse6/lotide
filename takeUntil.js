8c7e6404ccaf8e6d6d1acf05ba8becc841cd4eb4
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-06 20:24:23 +0000

    adding takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,26 @@
+const takeUntil = function(array, callback) {
+  let newArr = [];
+  for (let i of array) {
+    if (!callback(i)) {
+      newArr.push(i);
+    } else {
+      break;
+    }
+  }
+  return newArr;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+// [ 1, 2, 5, 7, 2 ]
+// ---
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
